4bd5fa2c2363df1b17e30ff6bd51f5f4879df910
Haikal Alfandi <kungkingkangcanyouseemekuking@gmail.com>
2023-10-23 22:00:25 -0400

    Finished

--- index.js
+++ index.js
@@ -22,3 +22,47 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+let arrLink = [];
+
+app.post('/api/shorturl', (req, res) => {
+  // res.json({
+  //   testUrl: req.body.url
+  // });
+  const regex = /^(http|https):\/\/[^ "]+$/;
+  if (regex.test(req.body.url)) {
+    // if the url alredy in arrLink display the array
+    if (arrLink.includes(req.body.url)) {
+      res.json({
+        original_url: req.body.url,
+        short_url: arrLink.indexOf(req.body.url)
+      });
+    }
+
+    // insert new map
+    arrLink.push({
+      original_url: req.body.url,
+      short_url: arrLink.length + 1
+    });
+    
+    // crete short url
+    res.json({
+      original_url: req.body.url,
+      short_url: arrLink.length
+    });
+    
+  } else {
+    res.json({
+      error: 'Invalid Url'
+    });
+  }
+
+});
+
+app.get('/api/shorturl/:short_url', (req,res) => {
+  const short_url = req.params.short_url;
+  const original_url = arrLink[short_url - 1].original_url;
+  res.redirect(original_url);
+});
+
